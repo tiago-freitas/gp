280813ee0a5593467b26f3ce6c4616f45ef3eeca
Tiago Barreiros de Freitas <tiago4680@gmail.com>
2023-12-29 16:47:26 -0300

    Rendering gridboard!

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 
 #include <SDL2/SDL.h>
 
+#define BOARD_WIDTH  10
+#define BOARD_HEIGHT 10
+
 #define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 640
+#define SCREEN_HEIGHT 800
+
+#define CELL_WIDTH ((float) SCREEN_WIDTH / BOARD_WIDTH)
+#define CELL_HEIGHT ((float) SCREEN_HEIGHT / BOARD_HEIGHT)
 
 int scc(int code)
 {
@@ -26,6 +32,34 @@ void *scp(void *ptr)
     return ptr;
 }
 
+void render_grid_board(SDL_Renderer *renderer)
+{
+    scc(SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255));
+    for (int x = 1; x < BOARD_WIDTH; ++x) {
+        scc(SDL_RenderDrawLine(
+                renderer,
+                CELL_WIDTH * x,
+                0,
+                CELL_WIDTH * x,
+                SCREEN_HEIGHT));
+    }
+    for (int y = 1; y < BOARD_HEIGHT; ++y) {
+        // printf("cell_height = %f, y = %d, cell*y = %f\n", CELL_HEIGHT, y, CELL_HEIGHT*y);
+        scc(SDL_RenderDrawLine(
+                renderer,
+                0,
+                CELL_HEIGHT * y,
+                SCREEN_WIDTH,
+                CELL_HEIGHT * y));
+    }
+
+    //     for (int y = 0; y < BOARD_HEIGHT; ++y) {
+    //         SDL_RenderDrawLine(renderer,
+    //                            int x1, int y1, int x2, int y2);
+    //     }
+    // }
+}
+
 int main(void)
 {
     scc(SDL_Init(SDL_INIT_VIDEO));
@@ -41,6 +75,11 @@ int main(void)
         -1,
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
 
+    scc(SDL_RenderSetLogicalSize(
+            renderer,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT));
+
     int quit = 0;
     while (!quit) {
         SDL_Event event;
@@ -54,7 +93,11 @@ int main(void)
 
         scc(SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255));
         scc(SDL_RenderClear(renderer));
+
+        render_grid_board(renderer);
+
         SDL_RenderPresent(renderer);
+ 
     }
 
 
