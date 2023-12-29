63aa23a4a5265dfb081daa8971ebe740f4d3f658
Tiago Barreiros de Freitas <tiago4680@gmail.com>
2023-12-29 15:26:14 -0300

    SDL first steps

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 640
+
+int scc(int code)
 {
-    printf("Olá, Mundo!\n");
+    if (code < 0) {
+        fprintf(stderr, "SDL error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    return code;
+}
+
+void *scp(void *ptr)
+{
+    if (ptr == NULL) {
+        fprintf(stderr, "SDL error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    return ptr;
+}
+
+int main(void)
+{
+    scc(SDL_Init(SDL_INIT_VIDEO));
+
+    SDL_Window *const window = scp(SDL_CreateWindow(
+        "Hunger Games",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_RESIZABLE));
+
+    SDL_Renderer *const renderer = scp(SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+
+    int quit = 0;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    quit = 1;
+                } break;
+            }
+        }
+
+        scc(SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255));
+        scc(SDL_RenderClear(renderer));
+        SDL_RenderPresent(renderer);
+    }
+
+
+
+    SDL_Quit();
     return 0;
 }
